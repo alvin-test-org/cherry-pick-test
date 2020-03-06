e5b6718360d8ddf5df710d75664836c83e225706
Alvin Huang <17609145+alvin-huang@users.noreply.github.com>
2020-03-05 22:13:07 -0500

    modify script

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 # search for the PR labels applicable to the specified commit
-labels=$(curl "https://api.github.com/search/issues?q=repo:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME+sha:$CIRCLE_SHA1" | jq --raw-output '.items[].labels[] | .name')
+resp=$(curl -f -s "https://api.github.com/search/issues?q=repo:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME+sha:$CIRCLE_SHA1")
+status="$?"
+echo status is "$status"
+if [[ "$status" -ne 0 ]]; then
+  echo "This commit was not found in the GitHub API or was not associated with a PR"
+  exit $status
+fi
+
+labels=$(echo "$resp" | jq --raw-output '.items[].labels[] | .name')
 
 for label in $labels; do
     echo "checking label: $label"
     if [[ $label =~ docs* ]]; then
         echo "docs"
-        git checkout stable-website
-        git cherry-pick $CIRCLE_SHA1
-        git push origin stable-website
+#        git checkout stable-website
+#        git cherry-pick $CIRCLE_SHA1
+#        git push origin stable-website
     elif [[ $label =~ backport/* ]]; then
         echo "backporting to $label"
         branch=${label/backport/release}
-        git checkout $branch
-        git cherry-pick $CIRCLE_SHA1
-        git push origin $branch
+#        git checkout $branch
+#        git cherry-pick $CIRCLE_SHA1
+#        git push origin $branch
     fi
 done
